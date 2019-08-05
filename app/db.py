54031f7e9623c85c3a938539153fa6715f500e33
Dmitriy Valetov <dmitriy.valetov@phystech.edu>
2019-08-05 03:14:55 +0300

    added promo codes to the base and binded them to teams

--- app/db.py
+++ app/db.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sqlite3
+import json
 import os
 
 root = os.path.split(__file__)[0]
@@ -26,29 +27,61 @@ class Event(Base):
 class Team(Base):
     __tablename__ = 'teams'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
+    name = Column(String, unique=True)
     ap = Column(Integer) # action points
+    promos = relationship('Promo')
     def __repr__(self):
         return "Team ({} : {} : {})".format(self.id, self.name, self.ap)
 
-class PromoAct(Base):
-    __tablename__ = 'PromoActs'
+class Promo(Base):
+    __tablename__ = 'promos'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    code = Column(String)
-    team = relationship("Team")
+    name = Column(String)
+    status = Column(String, default='off')
+    team_id = Column(Integer, ForeignKey('teams.id'))
+    ap = Column(Integer) # action points
     def __repr__(self):
-        return "PromoAct ({} : {} : {})".format(self.id, self.name, self.team)
+        return "Promo ({} : {} : {} : {})".format(self.id, self.name, self.status, self.team_id)
 
-def check_Promoact(promo_code, team_name):
+
+def get_info():
+    """
+    returns some db info
+    """
     engine = create_engine('sqlite:///{}'.format(os.path.join(root, 'reactor.db')))
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.add(PromoAct(name=hesouam, team=))
-    print('\n\n')
-    print(session.query(PromoAct.id).filter_by(code=promo_code).get(0))
-    print('\n\n')
-    promo_used = session.query(PromoAct.id).filter_by(code=promo_code).filter(PromoAct.team.has(name=team_name)).scalar() is not None
+    data = {'promo':[], 'teams':[]}
+    for team in session.query(Team).all():
+        data['teams'].append(str(team))
+    for promo in session.query(Promo).all():
+        data['promos'].append(str(promo))
     session.commit()
+    session.close()
+    engine.dispose()
+    return json.dumps(data)
+
+
+def promo_is_used(promo_name, team_name):
+    """
+    None - promo is no suitable for this comand
+    False - promo not used
+    True - promo already used
+    """
+    engine = create_engine('sqlite:///{}'.format(os.path.join(root, 'reactor.db')))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    promo_used = None
+    promos = session.query(Team).filter(Team.name == team_name).one().promos
+    for p in promos:
+        if p['name'] == promo_name:
+            if p.status == 'off':
+                promo_used = False
+            else:
+                promo_used = True
+
+    # session.commit() # no changes!
     session.close()
     engine.dispose()
     return promo_used
@@ -63,14 +96,28 @@ def increment_ap(team_name, incr_ap):
     session.close()
     engine.dispose()
 
-def add_PromoAct(promo_code, team_name):
+def toggle_promo(promo_name, team_name):
+    """
+    False - toggle failed
+    True - toggle success
+    """
     engine = create_engine('sqlite:///{}'.format(os.path.join(root, 'reactor.db')))
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.add(PromoAct(code=promo_code, team=team_name))
-    session.commit()
+
+    promo_toggled = False
+    promos = session.query(Team).filter(Team.name == team_name).one().promos
+    for p in promos:
+        if p['name'] == promo_name:
+            if p.status == 'off':
+                p.status = 'on'
+                promo_toggled = True
+                session.add(p)
+
+    session.commit() # no changes!
     session.close()
     engine.dispose()
+    return promo_toggled
 
 def init(app):
     """
@@ -85,6 +132,9 @@ def init(app):
         if not(team_exists):
             new_team = Team(name=team_name, ap=app.configs['init_ap'])
             session.add(new_team)
+            if team_name in app.configs['promos']:
+                for promo_pack in app.configs['promos'][team_name]:
+                    new_team.promos.append(Promo(name=promo_pack['name'], status='off', ap=promo_pack['ap']))
     session.commit()
     session.close()
     engine.dispose()
@@ -129,33 +179,33 @@ def init_solutons_db_sheme():
 #     engine.dispose()
 #     return x, y
 
-def init_db(path):
-    """
-    Init or drop reactor.db
-    There should be tables: states, events, teams
-    """
-    # clear tables
-    con = sqlite3.connect(os.path.join(path, 'reactor.db'))
-    cur = con.cursor()
-    cur.execute('''DROP TABLE IF EXISTS states''')
-    cur.execute('''CREATE TABLE IF NOT EXISTS states (
-    id integer PRIMARY KEY, data text)''')
-    cur.execute('''DROP TABLE IF EXISTS events''')
-    cur.execute('''CREATE TABLE IF NOT EXISTS events (
-    id integer PRIMARY KEY, data text)''')
-    con.close()
+# def init_db(path):
+#     """
+#     Init or drop reactor.db
+#     There should be tables: states, events, teams
+#     """
+#     # clear tables
+#     con = sqlite3.connect(os.path.join(path, 'reactor.db'))
+#     cur = con.cursor()
+#     cur.execute('''DROP TABLE IF EXISTS states''')
+#     cur.execute('''CREATE TABLE IF NOT EXISTS states (
+#     id integer PRIMARY KEY, data text)''')
+#     cur.execute('''DROP TABLE IF EXISTS events''')
+#     cur.execute('''CREATE TABLE IF NOT EXISTS events (
+#     id integer PRIMARY KEY, data text)''')
+#     con.close()
 
-def check_tables(path):
-    """
-    Check if tables exists: states, events, teams - create them
-    """
-    check_string = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';"
-    create_string = "CREATE TABLE IF NOT EXISTS {table_name} (id integer PRIMARY KEY, data text)"
-    with sqlite3.connect('./reactor.db') as con:
-        cur = con.cursor()
-        tables = ['states', 'events']
-        for tbl in tables:
-            formated_check_string = check_string.format(tbl)
-            cur.execute(formated_check_string)
-            if cur.fetchone() is None:
-                cur.execute(create_string.format(tbl))
+# def check_tables(path):
+#     """
+#     Check if tables exists: states, events, teams - create them
+#     """
+#     check_string = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';"
+#     create_string = "CREATE TABLE IF NOT EXISTS {table_name} (id integer PRIMARY KEY, data text)"
+#     with sqlite3.connect('./reactor.db') as con:
+#         cur = con.cursor()
+#         tables = ['states', 'events']
+#         for tbl in tables:
+#             formated_check_string = check_string.format(tbl)
+#             cur.execute(formated_check_string)
+#             if cur.fetchone() is None:
+#                 cur.execute(create_string.format(tbl))
